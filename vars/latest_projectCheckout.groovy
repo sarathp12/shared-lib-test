1980911d22f1edaf8fedc1702ea62b717e15f223
sarathp12 <psarathyp18@gmail.com>
2018-09-21 14:26:51 -0400

    update in git url, branch and credentials

--- vars/latest_projectCheckout.groovy
+++ vars/latest_projectCheckout.groovy
@@ -17,9 +17,7 @@ def call(String stageName, PipelineContext context) {
 	if (exists) {
 		sh("git clean -fd")
 	}
-	
-	git url: pipelineConfig['gitURL'], branch: "COPS-419" credentialsID['final-ele-app-connect']
-
+	git([url: pipelineConfig['gitURl'], branch: 'COPS-419', credentialsId: 'final-ele-app-connect'])
 	String commitHash = sh (returnStdout: true, script: "git rev-parse --short HEAD").trim()
 	String buildId = "${env.BUILD_NUMBER}-${commitHash}"
 	
