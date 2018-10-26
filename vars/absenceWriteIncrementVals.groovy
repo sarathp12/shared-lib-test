658ef09348467985be2a7e6654ce84a6617da7ee
root <root@localhost.localdomain>
2018-10-26 12:03:01 -0400

    write step added

--- vars/absenceWriteIncrementVals.groovy
+++ vars/absenceWriteIncrementVals.groovy
@@ -1,6 +1,7 @@
 #! /usr/bin/groovy
 def writeIncrementVals = {
 	File x = new File('build-version.properties')
+        x.createNewFile()
         x.write "# Property values when build-version.properties is not found\n"
         x << "major=0\n"
         x << "minor=0\n"
